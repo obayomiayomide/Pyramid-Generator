bea39fedaca55c7bd274a60520b6dd6b397743f1
obayomiayomide <obayomiayomide2020@gmail.com>
2024-07-09 21:01:44 +0100

    updated Javascript

--- script.js
+++ script.js
@@ -47,11 +47,6 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-// function padRow(rowNumber, rowCount) {
-//   character = characterInput.value;
-//   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
-// }
-
 // // Loop code 1
 // for (let i = 1; i <= count; i++) {
 //     if (inverted) {
@@ -61,23 +56,7 @@ function padRow(rowNumber, rowCount) {
 //     }
 // }
 
-
-// Loop code 1
-
 //Loop Code 1 for Inverted Pyramid
 // for (let i = count; i > 0; i--) {
 //     rows.push(padRow(i, count));
 // }
-
-//Loop Code 2 for Inverted Pyramid
-// for (let i = 1; i <= count; i++) {
-//     rows.unshift(padRow(i, count));
-// }
-
-// let result = ""
-
-// for (const row of rows) {
-//   result = result + "\n" + row;
-// }
-
-// console.log(result);
